74f79f157f7ac9ffd385a458e816d5321a416015
Mohammad Reza Fahimi <fang.rui@hotmail.com>
2025-09-12 16:32:38 -0400

    abb

--- E4/E4analysis.R
+++ E4/E4analysis.R
@@ -10,43 +10,43 @@ library(stringr)
 load("stats_matrix_e4.RData")
 
 model_type_labels <- c(
-  "adcat" = "AC",
-  "propodds" = "CL",
-  "stoprat" = "SR"
+  "adcat"="AC",
+  "propodds"="CL",
+  "stoprat"="SR"
 )
 
 stats_by_group$Sampling_Method <- factor(stats_by_group$Sampling_Method,
-                                         levels = c("SRS", "Best-SRS",
-                                                    "Z-Bal", "GA-Z-Bal",
-                                                    "Y-Bal", "GA-Y-Bal",
-                                                    "YZ-Bal", "GA-YZ-Bal"))
+                                         levels=c("SRS","Best-SRS",
+                                                    "Z-Bal","GA-Z-Bal",
+                                                    "Y-Bal","GA-Y-Bal",
+                                                    "YZ-Bal","GA-YZ-Bal"))
 # Web Figure 17
 p1 <- stats_by_group %>%
-  filter(G_vec == "G1", Experiment == 4, Sampling_Method %in% c("SRS",
+  filter(G_vec == "G1",Experiment == 4,Sampling_Method %in% c("SRS",
                                                                 "Z-Bal",
                                                                 "Y-Bal",
-                                                                "YZ-Bal"), BETA1 == 25) %>%
-  group_by(Sampling_Method, Analysis_Method, Sample_Size) %>%
-  ggplot(aes(x = Sample_Size, y = Mean_Absolute_Relative_Bias, shape = Analysis_Method, color = Analysis_Method, linetype = "Incomplete Data")) +
-  geom_line(linewidth = 1.2) +
-  geom_line(aes(y = Mean_Absolute_Relative_Bias_Complete, linetype = "Complete Data"), linewidth = 0.5, alpha = 0.4) +
-  geom_hline(yintercept = 0, color = "black", linetype = "dashed") +
+                                                                "YZ-Bal"),BETA1 == 25) %>%
+  group_by(Sampling_Method,Analysis_Method,Sample_Size) %>%
+  ggplot(aes(x=Sample_Size,y=Mean_Absolute_Relative_Bias,shape=Analysis_Method,color=Analysis_Method,linetype="Incomplete Data")) +
+  geom_line(linewidth=1.2) +
+  geom_line(aes(y=Mean_Absolute_Relative_Bias_Complete,linetype="Complete Data"),linewidth=0.5,alpha=0.4) +
+  geom_hline(yintercept=0,color="black",linetype="dashed") +
   theme_bw() +
-  facet_grid(Model_Type ~ Sampling_Method, labeller = labeller(Model_Type = model_type_labels)) + 
+  facet_grid(Model_Type ~ Sampling_Method,labeller=labeller(Model_Type=model_type_labels)) + 
   labs(
-    title = "Mean Absolute Relative Bias by Sample Size (Baseline)- Exp.4-B25",  
-    x = "Phase 2 Sample Size", 
-    y = "Mean Absolute Relative Bias",  
-    shape = "Analysis Method",  
-    color = "Analysis Method",  
-    linetype = "Data Completeness"
+    title="Mean Absolute Relative Bias by Sample Size (Baseline)- Exp.4-B25", 
+    x="Phase 2 Sample Size",
+    y="Mean Absolute Relative Bias", 
+    shape="Analysis Method", 
+    color="Analysis Method", 
+    linetype="Data Completeness"
   ) +
   scale_x_continuous(
-    breaks = c(500, 1500, 2500, 3500, 4500)
+    breaks=c(500,1500,2500,3500,4500)
   ) +
   theme(
-    axis.text.x = element_text(angle = 80, hjust = 1), legend.position = "bottom",
-    legend.box = "vertical"
+    axis.text.x=element_text(angle=80,hjust=1),legend.position="bottom",
+    legend.box="vertical"
   )
 
 
@@ -54,63 +54,63 @@ p1 <- stats_by_group %>%
 # Web Figure 18
 
 p1.1 <- stats_by_group %>%
-  filter(G_vec == "G1", Experiment == 4, Sampling_Method %in% c("Best-SRS",
+  filter(G_vec == "G1",Experiment == 4,Sampling_Method %in% c("Best-SRS",
                                                                 "GA-Z-Bal",
                                                                 "GA-Y-Bal",
-                                                                "GA-YZ-Bal"), BETA1 == 25) %>%
-  group_by(Sampling_Method, Analysis_Method, Sample_Size) %>%
-  ggplot(aes(x = Sample_Size, y = Mean_Absolute_Relative_Bias, shape = Analysis_Method, color = Analysis_Method, linetype = "Incomplete Data")) +
-  geom_line(linewidth = 1.2) +
-  geom_line(aes(y = Mean_Absolute_Relative_Bias_Complete, linetype = "Complete Data"), linewidth = 0.5, alpha = 0.4) +
-  geom_hline(yintercept = 0, color = "black", linetype = "dashed") +
+                                                                "GA-YZ-Bal"),BETA1 == 25) %>%
+  group_by(Sampling_Method,Analysis_Method,Sample_Size) %>%
+  ggplot(aes(x=Sample_Size,y=Mean_Absolute_Relative_Bias,shape=Analysis_Method,color=Analysis_Method,linetype="Incomplete Data")) +
+  geom_line(linewidth=1.2) +
+  geom_line(aes(y=Mean_Absolute_Relative_Bias_Complete,linetype="Complete Data"),linewidth=0.5,alpha=0.4) +
+  geom_hline(yintercept=0,color="black",linetype="dashed") +
   theme_bw() +
-  facet_grid(Model_Type ~ Sampling_Method, labeller = labeller(Model_Type = model_type_labels)) + 
+  facet_grid(Model_Type ~ Sampling_Method,labeller=labeller(Model_Type=model_type_labels)) + 
   labs(
-    title = "Mean Absolute Relative Bias by Sample Size (Optimal)- Exp.4-B25",  
-    x = "Phase 2 Sample Size", 
-    y = "Mean Absolute Relative Bias",  
-    shape = "Analysis Method",  
-    color = "Analysis Method",  
-    linetype = "Data Completeness"
+    title="Mean Absolute Relative Bias by Sample Size (Optimal)- Exp.4-B25", 
+    x="Phase 2 Sample Size",
+    y="Mean Absolute Relative Bias", 
+    shape="Analysis Method", 
+    color="Analysis Method", 
+    linetype="Data Completeness"
   ) +
   scale_x_continuous(
-    breaks = c(500, 1500, 2500, 3500, 4500)
+    breaks=c(500,1500,2500,3500,4500)
   ) +
   theme(
-    axis.text.x = element_text(angle = 80, hjust = 1), legend.position = "bottom",
-    legend.box = "vertical"
+    axis.text.x=element_text(angle=80,hjust=1),legend.position="bottom",
+    legend.box="vertical"
   )
 
 
 # Web Figure 19
 
 p1.3 <- stats_by_group %>%
-  filter(G_vec == "G1", Experiment == 4, Sampling_Method %in% c("Best-SRS",
+  filter(G_vec == "G1",Experiment == 4,Sampling_Method %in% c("Best-SRS",
                                                                 "GA-Z-Bal",
                                                                 "GA-Y-Bal",
                                                                 "GA-YZ-Bal"),
-         Analysis_Method == "EM", BETA1 == 25) %>%
-  group_by(Sampling_Method, Sample_Size) %>%
-  ggplot(aes(x = Sample_Size, y = Mean_Absolute_Relative_Bias, linetype = "Incomplete Data")) +
-  geom_line(linewidth = 1.2) +
-  geom_line(aes(y = Mean_Absolute_Relative_Bias_Complete, linetype = "Complete Data"), linewidth = 0.5, alpha = 0.4) +
-  geom_hline(yintercept = 0, color = "red", linetype = "dashed") +
+         Analysis_Method == "EM",BETA1 == 25) %>%
+  group_by(Sampling_Method,Sample_Size) %>%
+  ggplot(aes(x=Sample_Size,y=Mean_Absolute_Relative_Bias,linetype="Incomplete Data")) +
+  geom_line(linewidth=1.2) +
+  geom_line(aes(y=Mean_Absolute_Relative_Bias_Complete,linetype="Complete Data"),linewidth=0.5,alpha=0.4) +
+  geom_hline(yintercept=0,color="red",linetype="dashed") +
   theme_bw() +
-  facet_grid(Model_Type ~ Sampling_Method, labeller = labeller(Model_Type = model_type_labels)) + 
+  facet_grid(Model_Type ~ Sampling_Method,labeller=labeller(Model_Type=model_type_labels)) + 
   labs(
-    title = "Mean Absolute Relative Bias by Sample Size (Optimal-EM only)- Exp.4-B25",  
-    x = "Phase 2 Sample Size", 
-    y = "Mean Absolute Relative Bias",  
-    shape = "Analysis Method",  
-    color = "Analysis Method",  
-    linetype = "Data Completeness"
+    title="Mean Absolute Relative Bias by Sample Size (Optimal-EM only)- Exp.4-B25", 
+    x="Phase 2 Sample Size",
+    y="Mean Absolute Relative Bias", 
+    shape="Analysis Method", 
+    color="Analysis Method", 
+    linetype="Data Completeness"
   ) +
   scale_x_continuous(
-    breaks = c(500, 1500, 2500, 3500, 4500)
+    breaks=c(500,1500,2500,3500,4500)
   ) +
   theme(
-    axis.text.x = element_text(angle = 80, hjust = 1), legend.position = "bottom",
-    legend.box = "vertical"
+    axis.text.x=element_text(angle=80,hjust=1),legend.position="bottom",
+    legend.box="vertical"
   )
 
 
@@ -121,26 +121,26 @@ p1.3 <- stats_by_group %>%
 # Web Figure 20
 
 p3 <- stats_by_group %>%
-  filter(G_vec == "G1", Experiment == 4, Sampling_Method != "Complete_Data", BETA1 == 25) %>%
-  group_by(Sampling_Method, Analysis_Method, Sample_Size) %>%
-  ggplot(aes(x = Sample_Size, y = Relative_Efficiency, shape = Analysis_Method, color = Analysis_Method, linetype = Analysis_Method)) +
+  filter(G_vec == "G1",Experiment == 4,Sampling_Method != "Complete_Data",BETA1 == 25) %>%
+  group_by(Sampling_Method,Analysis_Method,Sample_Size) %>%
+  ggplot(aes(x=Sample_Size,y=Relative_Efficiency,shape=Analysis_Method,color=Analysis_Method,linetype=Analysis_Method)) +
   geom_line(linewidth=1) +
-  geom_hline(yintercept = 1, color = "black", linetype = "dashed") +
+  geom_hline(yintercept=1,color="black",linetype="dashed") +
   theme_bw ()  +
-  facet_grid(Model_Type ~ Sampling_Method, labeller = labeller(Model_Type = model_type_labels))  + 
+  facet_grid(Model_Type ~ Sampling_Method,labeller=labeller(Model_Type=model_type_labels))  + 
   labs(
-    title = "MSE Ratio - Exp.4-B25",  
-    x = "Phase 2 Sample Size", 
-    y = "MSE Ratio",  
-    shape = "Analysis Method",  
-    color = "Analysis Method",  
-    linetype = "Analysis Method"  
+    title="MSE Ratio - Exp.4-B25", 
+    x="Phase 2 Sample Size",
+    y="MSE Ratio", 
+    shape="Analysis Method", 
+    color="Analysis Method", 
+    linetype="Analysis Method"  
   )  +
   scale_x_continuous(
-    breaks = c(500,  1500,  2500,  3500,  4500)  ) +
+    breaks=c(500, 1500, 2500, 3500, 4500)  ) +
   theme(
-    axis.text.x = element_text(angle = 80, hjust = 1), legend.position = "bottom",
-    legend.box = "vertical")
+    axis.text.x=element_text(angle=80,hjust=1),legend.position="bottom",
+    legend.box="vertical")
 
 
 
@@ -154,82 +154,82 @@ library(dplyr)
 library(purrr)
 library(stats)
 
-calculate_power_type1_error <- function(data, alpha, statistic) {
-  critical_value <- qchisq(1 - alpha, df = 1)
+calculate_power_type1_error <- function(data,alpha,statistic) {
+  critical_value <- qchisq(1 - alpha,df=1)
   
   statistic <- sym(statistic)
   
  
   
-  clopper_pearson_ci <- function(successes, n, alpha = 0.05) {
-    lower <- qbeta(alpha / 2, successes, n - successes + 1)
-    upper <- qbeta(1 - alpha / 2, successes + 1, n - successes)
-    return(c(lower = lower, upper = upper))
+  clopper_pearson_ci <- function(successes,n,alpha=0.05) {
+    lower <- qbeta(alpha / 2,successes,n - successes + 1)
+    upper <- qbeta(1 - alpha / 2,successes + 1,n - successes)
+    return(c(lower=lower,upper=upper))
   }
   
   
   type1_errors <- data %>%
-    filter(G_vec == "G0", Sample_Size %in% c(500,1000,1500,2000,2500)) %>%
-    group_by(Sample_Size, Experiment, Sampling_Strategy, Method, Model_Type, BETA1) %>%
+    filter(G_vec == "G0",Sample_Size %in% c(500,1000,1500,2000,2500)) %>%
+    group_by(Sample_Size,Experiment,Sampling_Strategy,Method,Model_Type,BETA1) %>%
     summarise(
-      Trials = n(),
-      Successes = sum(!!statistic > critical_value, na.rm = TRUE),
-      Type1_Error_Rate = Successes / Trials,
-      .groups = 'drop'
+      Trials=n(),
+      Successes=sum(!!statistic > critical_value,na.rm=TRUE),
+      Type1_Error_Rate=Successes / Trials,
+      .groups='drop'
     ) %>%
     mutate(
-      CI = map2(Successes, Trials, clopper_pearson_ci),
-      CI_Lower = map_dbl(CI, "lower"),
-      CI_Upper = map_dbl(CI, "upper")
+      CI=map2(Successes,Trials,clopper_pearson_ci),
+      CI_Lower=map_dbl(CI,"lower"),
+      CI_Upper=map_dbl(CI,"upper")
     ) %>%
     dplyr::select(-CI)
   
   power <- data %>%
-    filter(G_vec == "G1", Sample_Size %in% c(500,1000,1500,2000,2500)) %>%
-    group_by(Sample_Size, Experiment, Sampling_Strategy, Method, Model_Type, BETA1) %>%
+    filter(G_vec == "G1",Sample_Size %in% c(500,1000,1500,2000,2500)) %>%
+    group_by(Sample_Size,Experiment,Sampling_Strategy,Method,Model_Type,BETA1) %>%
     summarise(
-      Trials = n(),
-      Successes = sum(!!statistic > critical_value, na.rm = TRUE),
-      Power = Successes / Trials,
-      .groups = 'drop'
+      Trials=n(),
+      Successes=sum(!!statistic > critical_value,na.rm=TRUE),
+      Power=Successes / Trials,
+      .groups='drop'
     ) %>%
     mutate(
-      CI = map2(Successes, Trials, clopper_pearson_ci),
-      CI_Lower = map_dbl(CI, "lower"),
-      CI_Upper = map_dbl(CI, "upper")
+      CI=map2(Successes,Trials,clopper_pearson_ci),
+      CI_Lower=map_dbl(CI,"lower"),
+      CI_Upper=map_dbl(CI,"upper")
     ) %>%
     dplyr::select(-CI)
   
   coverage <- data %>%
     filter(Sample_Size %in% c(500,1000,1500,2000,2500)) %>%
-    group_by(Sample_Size, Experiment, Sampling_Strategy, Method, Model_Type, BETA1) %>%
+    group_by(Sample_Size,Experiment,Sampling_Strategy,Method,Model_Type,BETA1) %>%
     summarise(
-      Trials = n(),
-      Successes = sum((True_Beta1 >= beta1 - 1.96 * sqrt(var_beta1)) & (True_Beta1 <= beta1 + 1.96 * sqrt(var_beta1)), na.rm = TRUE),
-      Coverage = Successes / Trials,
-      .groups = 'drop'
+      Trials=n(),
+      Successes=sum((True_Beta1 >= beta1 - 1.96 * sqrt(var_beta1)) & (True_Beta1 <= beta1 + 1.96 * sqrt(var_beta1)),na.rm=TRUE),
+      Coverage=Successes / Trials,
+      .groups='drop'
     ) %>%
     mutate(
-      CI = map2(Successes, Trials, clopper_pearson_ci),
-      Coverage_CI_Lower = map_dbl(CI, "lower"),
-      Coverage_CI_Upper = map_dbl(CI, "upper")
+      CI=map2(Successes,Trials,clopper_pearson_ci),
+      Coverage_CI_Lower=map_dbl(CI,"lower"),
+      Coverage_CI_Upper=map_dbl(CI,"upper")
     ) %>%
     dplyr::select(-CI)
   
-  # Combine Type 1 Error, Power, and Coverage into a single result
-  combined_results <- full_join(type1_errors, power, by = c("Sample_Size", "Sampling_Strategy", "Experiment", "Method", "Model_Type", "BETA1")) %>%
-    full_join(coverage, by = c("Sample_Size", "Sampling_Strategy", "Experiment", "Method", "Model_Type", "BETA1"))
+  # Combine Type 1 Error,Power,and Coverage into a single result
+  combined_results <- full_join(type1_errors,power,by=c("Sample_Size","Sampling_Strategy","Experiment","Method","Model_Type","BETA1")) %>%
+    full_join(coverage,by=c("Sample_Size","Sampling_Strategy","Experiment","Method","Model_Type","BETA1"))
   
   return(combined_results)
 }
 
-power_type1_error_results_0.05_LR <- calculate_power_type1_error(all_combined, 0.05, "LR")
+power_type1_error_results_0.05_LR <- calculate_power_type1_error(all_combined,0.05,"LR")
 power_type1_error_results_0.05_LR$Test_Statistic <- "LR"
 
-power_type1_error_results_0.05_W <- calculate_power_type1_error(all_combined, 0.05, "W")
+power_type1_error_results_0.05_W <- calculate_power_type1_error(all_combined,0.05,"W")
 power_type1_error_results_0.05_W$Test_Statistic <- "Wald"
 
-power_type1_error_results_0.05_S <- calculate_power_type1_error(all_combined, 0.05, "S")
+power_type1_error_results_0.05_S <- calculate_power_type1_error(all_combined,0.05,"S")
 power_type1_error_results_0.05_S$Test_Statistic <- "Score"
 
 power_type1_error_results_0.05_all_tests <- rbind(power_type1_error_results_0.05_LR,power_type1_error_results_0.05_W,power_type1_error_results_0.05_S)
@@ -237,7 +237,7 @@ power_type1_error_results_0.05_all_tests <- rbind(power_type1_error_results_0.05
 
 power_type1_error_results_0.05_all_tests <- power_type1_error_results_0.05_all_tests %>%
   mutate(
-    Sampling_Method = case_when(
+    Sampling_Method=case_when(
       Method == "Balanced-EM" ~ "Y-Bal",
       Method == "Balanced-Naive" ~ "Y-Bal",
       Method == "SRS-EM" ~ "SRS",
@@ -257,45 +257,45 @@ power_type1_error_results_0.05_all_tests <- power_type1_error_results_0.05_all_t
       Method == "Complete" ~ "Complete_Data",
       TRUE ~ NA_character_
     ),
-    Analysis_Method = case_when(
-      str_detect(Method, "EM") ~ "EM",
-      str_detect(Method, "Naive") ~ "Naive",
+    Analysis_Method=case_when(
+      str_detect(Method,"EM") ~ "EM",
+      str_detect(Method,"Naive") ~ "Naive",
       Method == "Complete" ~ "Complete",
       TRUE ~ NA_character_
     )
   ) %>%
   mutate(
-    Sampling_Method = factor(Sampling_Method, levels = c("SRS", "Best-SRS",
-                                                         "Z-Bal", "GA-Z-Bal",
-                                                         "Y-Bal", "GA-Y-Bal",
-                                                         "YZ-Bal", "GA-YZ-Bal", "Complete_Data"))
+    Sampling_Method=factor(Sampling_Method,levels=c("SRS","Best-SRS",
+                                                         "Z-Bal","GA-Z-Bal",
+                                                         "Y-Bal","GA-Y-Bal",
+                                                         "YZ-Bal","GA-YZ-Bal","Complete_Data"))
   )
 
 
 # Web Figure 21
 Power_Effect_E1_n500 <- power_type1_error_results_0.05_all_tests %>%
-  filter(Test_Statistic == "LR", Experiment == 4, Sample_Size == 500, Analysis_Method != "Complete", Sampling_Method != "Complete") %>%
-  group_by(Sampling_Method, Analysis_Method) %>%
-  ggplot(aes(x = BETA1, y = Power, color = Analysis_Method)) +
+  filter(Test_Statistic == "LR",Experiment == 4,Sample_Size == 500,Analysis_Method != "Complete",Sampling_Method != "Complete") %>%
+  group_by(Sampling_Method,Analysis_Method) %>%
+  ggplot(aes(x=BETA1,y=Power,color=Analysis_Method)) +
   geom_line() + 
-  geom_ribbon(aes(ymin = CI_Lower.y, ymax = CI_Upper.y, fill = Analysis_Method), alpha = 0.3, color = NA) +
-  facet_grid(Model_Type ~ Sampling_Method, labeller = labeller(Model_Type = model_type_labels)) +
+  geom_ribbon(aes(ymin=CI_Lower.y,ymax=CI_Upper.y,fill=Analysis_Method),alpha=0.3,color=NA) +
+  facet_grid(Model_Type ~ Sampling_Method,labeller=labeller(Model_Type=model_type_labels)) +
   labs(
-    title = "Power vs Effect Size - Exp.4 (n=500)",
-    x = "Effect Size",
-    y = "Power",
-    color = "Analysis Method",
-    fill = "Analysis Method"
+    title="Power vs Effect Size - Exp.4 (n=500)",
+    x="Effect Size",
+    y="Power",
+    color="Analysis Method",
+    fill="Analysis Method"
   )  +
   scale_x_continuous(
-    breaks = c(15, 20, 25, 30, 35),
-    labels = c("B15", "B20", "B25", "B30", "B35")
+    breaks=c(15,20,25,30,35),
+    labels=c("B15","B20","B25","B30","B35")
   ) +
-  ylim(0.15, 1.02) +
+  ylim(0.15,1.02) +
   theme_bw() +
   theme(
-    axis.text.x = element_text(angle = 80, hjust = 1), legend.position = "bottom",
-    legend.box = "vertical")
+    axis.text.x=element_text(angle=80,hjust=1),legend.position="bottom",
+    legend.box="vertical")
 
 
 
--- E5/E5wave1results.R
+++ E5/E5wave1results.R
@@ -4,25 +4,25 @@ load("w1analysis_dat.RData")
 
 library(ggplot2)
 
-ac_jp <- ggplot(adcat_table_bias_df, aes(x = w1samplesize, y = SumMeanAbsBias, color = Method)) +
-  geom_line(size = 1.2) +
-  geom_point(size = 2) +
-  geom_hline(yintercept = 0, linetype = "dashed") +
+ac_jp <- ggplot(adcat_table_bias_df, aes(x=w1samplesize, y=SumMeanAbsBias, color=Method)) +
+  geom_line(size=1.2) +
+  geom_point(size=2) +
+  geom_hline(yintercept=0, linetype="dashed") +
   labs(
-    title = "Adjacent-Category: Sum Mean\nAbsolute Bias for Joint Probabilities",
-    x = "Wave 1 Sample Size",
-    y = "Sum of Mean Absolute Bias"
+    title="Adjacent-Category: Sum Mean\nAbsolute Bias for Joint Probabilities",
+    x="Wave 1 Sample Size",
+    y="Sum of Mean Absolute Bias"
   ) 
 
-ac_bes <- ggplot(adcat_g_bias_df, aes(x = w1samplesize, y = MeanAbsBias, color = Method)) +
-  geom_line(size = 1.2) +
-  geom_point(size = 2) +
+ac_bes <- ggplot(adcat_g_bias_df, aes(x=w1samplesize, y=MeanAbsBias, color=Method)) +
+  geom_line(size=1.2) +
+  geom_point(size=2) +
   
-  geom_hline(yintercept = 0, linetype = "dashed") +
+  geom_hline(yintercept=0, linetype="dashed") +
   labs(
-    title = "Adjacent-Category: Mean\nAbsolute Relative Bias for Beta_1 Estimates",
-    x = "Wave 1 Sample Size",
-    y = "Mean Absolute Bias"
+    title="Adjacent-Category: Mean\nAbsolute Relative Bias for Beta_1 Estimates",
+    x="Wave 1 Sample Size",
+    y="Mean Absolute Bias"
   ) 
 
 
@@ -44,24 +44,24 @@ ac_bes <- ggplot(adcat_g_bias_df, aes(x = w1samplesize, y = MeanAbsBias, color =
 ###############################
 
 
-cl_jp <- ggplot(propodds_table_bias_df, aes(x = w1samplesize, y = SumMeanAbsBias, color = Method)) +
-  geom_line(size = 1.2) +
-  geom_point(size = 2) +
-  geom_hline(yintercept = 0, linetype = "dashed") +
+cl_jp <- ggplot(propodds_table_bias_df, aes(x=w1samplesize, y=SumMeanAbsBias, color=Method)) +
+  geom_line(size=1.2) +
+  geom_point(size=2) +
+  geom_hline(yintercept=0, linetype="dashed") +
   labs(
-    title = "Cumulative-Logit: Sum Mean\nAbsolute Bias for Joint Probabilities",
-    x = "Wave 1 Sample Size",
-    y = "Sum of Mean Absolute Bias"
+    title="Cumulative-Logit: Sum Mean\nAbsolute Bias for Joint Probabilities",
+    x="Wave 1 Sample Size",
+    y="Sum of Mean Absolute Bias"
   ) 
 
-cl_bes <- ggplot(propodds_g_bias_df, aes(x = w1samplesize, y = MeanAbsBias, color = Method)) +
-  geom_line(size = 1.2) +
-  geom_point(size = 2) +
-  geom_hline(yintercept = 0, linetype = "dashed") +
+cl_bes <- ggplot(propodds_g_bias_df, aes(x=w1samplesize, y=MeanAbsBias, color=Method)) +
+  geom_line(size=1.2) +
+  geom_point(size=2) +
+  geom_hline(yintercept=0, linetype="dashed") +
   labs(
-    title = "Cumulative-Logit: Mean Absolute\n Relative Bias for Beta_1 Estimates",
-    x = "Wave 1 Sample Size",
-    y = "Mean Absolute Bias"
+    title="Cumulative-Logit: Mean Absolute\n Relative Bias for Beta_1 Estimates",
+    x="Wave 1 Sample Size",
+    y="Mean Absolute Bias"
   ) 
 
 
@@ -93,24 +93,24 @@ cl_bes <- ggplot(propodds_g_bias_df, aes(x = w1samplesize, y = MeanAbsBias, colo
 
 
 
-sr_jp <- ggplot(stoprat_table_bias_df, aes(x = w1samplesize, y = SumMeanAbsBias, color = Method)) +
-  geom_line(size = 1.2) +
-  geom_point(size = 2) +
-  geom_hline(yintercept = 0, linetype = "dashed") +
+sr_jp <- ggplot(stoprat_table_bias_df, aes(x=w1samplesize, y=SumMeanAbsBias, color=Method)) +
+  geom_line(size=1.2) +
+  geom_point(size=2) +
+  geom_hline(yintercept=0, linetype="dashed") +
   labs(
-    title = "Stopping-Ratio: Sum Mean\nAbsolute Bias for Joint Probabilities",
-    x = "Wave 1 Sample Size",
-    y = "Sum of Mean Absolute Bias"
+    title="Stopping-Ratio: Sum Mean\nAbsolute Bias for Joint Probabilities",
+    x="Wave 1 Sample Size",
+    y="Sum of Mean Absolute Bias"
   ) 
 
-sr_bes <- ggplot(stoprat_g_bias_df, aes(x = w1samplesize, y = MeanAbsBias, color = Method)) +
-  geom_line(size = 1.2) +
-  geom_point(size = 2) +
-  geom_hline(yintercept = 0, linetype = "dashed") +
+sr_bes <- ggplot(stoprat_g_bias_df, aes(x=w1samplesize, y=MeanAbsBias, color=Method)) +
+  geom_line(size=1.2) +
+  geom_point(size=2) +
+  geom_hline(yintercept=0, linetype="dashed") +
   labs(
-    title = "Stopping-Ratio: Mean Absolute\n Relative Bias for Beta_1 Estimates",
-    x = "Wave 1 Sample Size",
-    y = "Mean Absolute Bias"
+    title="Stopping-Ratio: Mean Absolute\n Relative Bias for Beta_1 Estimates",
+    x="Wave 1 Sample Size",
+    y="Mean Absolute Bias"
   ) 
 
 
@@ -130,59 +130,59 @@ library(dplyr)
 
 
 g_bias_df <- g_bias_df %>%
-  mutate(Method = recode(Method,
-                         "YBal" = "Y-Bal",
-                         "YZBal" = "YZ-Bal",
-                         "ZBal" = "Z-Bal"))
+  mutate(Method=recode(Method,
+                         "YBal"="Y-Bal",
+                         "YZBal"="YZ-Bal",
+                         "ZBal"="Z-Bal"))
 
 table_bias_df <- table_bias_df %>%
-  mutate(Method = recode(Method,
-                         "YBal" = "Y-Bal",
-                         "YZBal" = "YZ-Bal",
-                         "ZBal" = "Z-Bal"))
+  mutate(Method=recode(Method,
+                         "YBal"="Y-Bal",
+                         "YZBal"="YZ-Bal",
+                         "ZBal"="Z-Bal"))
 
 ################################################################################
 
 
 
 
-table_plot <- ggplot(table_bias_df, aes(x = w1samplesize, y = SumMeanAbsBias, color = Method)) +
-  geom_line(aes(linetype = Method), size = 0.8) +
-  #geom_point(size = 1.2) +
+table_plot <- ggplot(table_bias_df, aes(x=w1samplesize, y=SumMeanAbsBias, color=Method)) +
+  geom_line(aes(linetype=Method), size=0.8) +
+  #geom_point(size=1.2) +
   facet_grid(. ~ Model) +
-  geom_hline(yintercept = 0, linetype = "dashed") +
+  geom_hline(yintercept=0, linetype="dashed") +
   labs(
-    title = "A) Mean Absolute Bias for Joint Probabilities",
-    x = "Wave 1 Sample Size",
-    y = "Mean Absolute Bias"
+    title="A) Mean Absolute Bias for Joint Probabilities",
+    x="Wave 1 Sample Size",
+    y="Mean Absolute Bias"
   ) + 
   theme_bw() +
-  theme(strip.text = element_text(size = 12, face = "bold"),
-        legend.position = "none")
+  theme(strip.text=element_text(size=12, face="bold"),
+        legend.position="none")
 
 
 
 
-g_bias_plot <- ggplot(g_bias_df, aes(x = w1samplesize, y = MeanAbsBias, color = Method)) +
-  geom_line(aes(linetype = Method), size = 0.8) +
-  #geom_point(size = 1.2) +
+g_bias_plot <- ggplot(g_bias_df, aes(x=w1samplesize, y=MeanAbsBias, color=Method)) +
+  geom_line(aes(linetype=Method), size=0.8) +
+  #geom_point(size=1.2) +
   facet_grid(. ~ Model) +
-  geom_hline(yintercept = 0, linetype = "dashed") +
+  geom_hline(yintercept=0, linetype="dashed") +
   labs(
-    title = bquote("B) Mean Absolute Relative Bias for " ~ beta[1]),
-    x = "Wave 1 Sample Size",
-    y = "Mean Absolute Bias"
+    title=bquote("B) Mean Absolute Relative Bias for " ~ beta[1]),
+    x="Wave 1 Sample Size",
+    y="Mean Absolute Bias"
   ) +
   theme_bw() +
   theme(
-    strip.text = element_text(size = 12, face = "bold"),
-    legend.position = "bottom",              
-    legend.title = element_text(size = 11),
-    legend.text = element_text(size = 10),
-    legend.box = "horizontal"                
+    strip.text=element_text(size=12, face="bold"),
+    legend.position="bottom",              
+    legend.title=element_text(size=11),
+    legend.text=element_text(size=10),
+    legend.box="horizontal"                
   )
 
 library(gridExtra)
 
 # Paper's Figure 1
-grid.arrange(table_plot, g_bias_plot, ncol = 1)
+grid.arrange(table_plot, g_bias_plot, ncol=1)
